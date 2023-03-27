8dec5555ebdcfc284bb368c204c80968be37bbaa
Kurubaran Anandhan <akurubaran@aol.com>
2023-03-27 08:15:14 +0530

    CREATED SETTINGS
    With the assistance of the "shared_preferences" dependency, I created a file called "settings.dart" to share theme settings.

---
+++ lib/utils/common/settings.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// Importing Shared Preferences
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Settings extends ChangeNotifier {
+  final SharedPreferences sharedPreferences;
+
+  Settings(this.sharedPreferences);
+
+  bool get isDarkMode => sharedPreferences?.getBool("isDarkMode") ?? false;
+
+  void setDarkMode(bool val) {
+    sharedPreferences?.setBool("isDarkMode", val);
+    notifyListeners();
+  }
+}
